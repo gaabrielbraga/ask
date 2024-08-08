937a7313287a0844cf68b296973994b5f094004b
Gabriel Braga <gabrieelbraaga@gmail.com>
2024-08-07 23:03:28 -0300

    feat: room page
    Signed-off-by: Gabriel Braga <gabrieelbraaga@gmail.com>

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,9 @@
-import { ArrowRight } from 'lucide-react';
-import { customAlphabet } from 'nanoid';
+import { ArrowRight } from "lucide-react";
+import { customAlphabet } from "nanoid";
+import { Logotipo } from "./components/Logotipo";
 
 function App() {
-  const nanoid = customAlphabet('1234567890abcdefghijklmnopqrstuvwxyz', 10);
+  const nanoid = customAlphabet("1234567890abcdefghijklmnopqrstuvwxyz", 10);
 
   const generateRoom = () => {
     const roomId = nanoid();
@@ -10,26 +11,21 @@ function App() {
   };
 
   return (
-    <main className='h-screen flex items-center justify-center'>
-      <div className='flex flex-col items-center justify-center gap-6'>
-        <img
-          src='../public/ask.me-logotipo.png'
-          alt='logotipo do ask.me'
-          width={180}
-          height={180}
-        />
-        <p className='text-center text-base font-normal leading-6 text-zinc-300 max-w-[450px]'>
+    <main className="h-screen flex items-center justify-center">
+      <div className="flex flex-col items-center justify-center gap-6">
+        <Logotipo />
+        <p className="text-center text-base font-normal leading-6 text-zinc-300 max-w-[450px]">
           Crie uma sala pública do ask.me e responda as perguntas mais
           importantes.
         </p>
-        <div className='flex min-w-[450px] p-2 justify-between items-center rounded-xl border gap-2 border-zinc-800 bg-zinc-900'>
+        <div className="flex min-w-[450px] p-2 justify-between items-center rounded-xl border gap-2 border-zinc-800 bg-zinc-900">
           <input
-            className='w-full h-full placeholder:text-zinc-500 text-sm font-normal bg-transparent flex-1'
-            placeholder='Nome da sala'
+            className="w-full h-full placeholder:text-zinc-500 text-sm font-normal bg-transparent flex-1"
+            placeholder="Nome da sala"
           />
           <button
             onClick={generateRoom}
-            className='bg-green-600 text-zinc-900 hover:bg-green-500 text-sm font-medium px-4 py-2 rounded-lg flex flex-row gap-1 items-center'
+            className="bg-green-600 text-zinc-900 hover:bg-green-500 text-sm font-medium px-4 py-2 rounded-lg flex flex-row gap-1 items-center"
           >
             Criar sala <ArrowRight size={16} />
           </button>
--- src/pages/Room/index.tsx
+++ src/pages/Room/index.tsx
@@ -1,30 +1,40 @@
-import { Share2 } from 'lucide-react';
-import { useParams } from 'react-router-dom';
+import { ArrowRight, Share2 } from "lucide-react";
+import { useParams } from "react-router-dom";
+import { Logotipo } from "../../components/Logotipo";
 
 export function Room() {
   const { roomId } = useParams();
 
   return (
-    <main className='h-screen flex items-center justify-center '>
-      <div className='flex flex-row items-center justify-center gap-36'>
-        <div className='flex flex-row items-center justify-center gap-2'>
-          <img
-            src='../public/ask.me-logotipo.png'
-            alt='logotipo do ask.me'
-            width={80}
-            height={80}
-          />
-          <p className='text-center text-sm font-normal leading-normal text-ellipsis text-zinc-500'>
-            Código da sala:{' '}
-            <span className='text-ellipsis text-sm font-normal leading-normal text-zinc-300'>
-              {roomId}
-            </span>
-          </p>
+    <body className="h-screen flex flex-col items-center mt-6 mx-0">
+      <div className="max-w-[580px] min-w-[580px] flex flex-col gap-6">
+        <div className="flex flex-row items-center justify-between gap-36 w-full">
+          <div className="flex flex-row items-center gap-2">
+            <Logotipo className="w-20 h-20" />
+            <p className="text-center text-sm font-normal leading-normal text-ellipsis text-zinc-500">
+              Código da sala:{" "}
+              <span className="text-ellipsis text-sm font-normal leading-normal text-zinc-300">
+                {roomId}
+              </span>
+            </p>
+          </div>
+          <button className="bg-zinc-800 flex justify-center items-center gap-3 px-3 py-2 rounded-lg hover:bg-zinc-700">
+            Compartilhar <Share2 size={16} />
+          </button>
         </div>
-        <button className='bg-zinc-800 flex justify-center items-center gap-3 px-3 py-2 rounded-lg hover:bg-zinc-700'>
-          Compartilhar <Share2 size={16} />
-        </button>
+
+        <hr className="border border-zinc-900" />
+
+        <div className="flex p-2 justify-between items-center rounded-xl border gap-2 border-zinc-800 bg-zinc-900 min-w-[520px]">
+          <input
+            className="w-full h-full placeholder:text-zinc-500 text-sm font-normal bg-transparent flex-1"
+            placeholder="Qual a sua pergunta?"
+          />
+          <button className="bg-green-600 text-zinc-900 hover:bg-green-500 text-sm font-medium px-4 py-2 rounded-lg flex flex-row gap-1 items-center">
+            Criar pergunta <ArrowRight size={16} />
+          </button>
+        </div>
       </div>
-    </main>
+    </body>
   );
 }
